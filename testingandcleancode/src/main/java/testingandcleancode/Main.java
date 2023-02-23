920709b498a8480dfcb7c32854385bda49c97a6a
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-02-22 20:40:11 -0500

    Testing&CleanCode - Lucas Moy - #3 Mockito
    -simular el comportamiento de BBDD, scanner, random, etc.

--- testingandcleancode/src/main/java/testingandcleancode/Main.java
+++ testingandcleancode/src/main/java/testingandcleancode/Main.java
@@ -3,7 +3,7 @@ package testingandcleancode;
 public class Main {
 
 	public static void main(String[] args) {
-		double salary = 1000d;
-		System.out.println(MoneyUtil.format(salary));
+		Game game = new Game();
+		game.play();
 	}
 }
